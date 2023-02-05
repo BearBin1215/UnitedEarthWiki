a831b0b15e27b951c6ccce22619ea32ae758ae3a
BearBin <460072919@qq.com>
2023-02-06 01:29:29 +0800

    Create PurgeCache.js

---
+++ PurgeCache.js
@@ -0,0 +1,55 @@
+'use strict';
+$(function () {
+    var containerNode;
+    if (mw.config.get("wgNamespaceNumber") === -1) {
+        containerNode = $('<span class="special-page"/>');
+        containerNode.append("特殊页面");
+    } else {
+        containerNode = $('<a class="purge-button"/>');
+        var statusNode = $('<span/>').text('清除缓存'),
+            runningStatus = false;
+        containerNode.attr("href", 'javascript:void(0);');
+        containerNode.append(statusNode);
+        containerNode.on('click', function () {
+            if (runningStatus) return;
+            statusNode.text('正在清除…');
+            runningStatus = true;
+            var api = new mw.Api(),
+                opt = {
+                    action: 'purge',
+                    format: 'json',
+                    forcelinkupdate: true,
+                    titles: mw.config.get('wgPageName')
+                };
+            api.post(opt).then(function () {
+                requestAnimationFrame(function () {
+                    api.post(opt).then(function () {
+                        statusNode.text('清除成功！');
+                        setTimeout(location.reload.bind(location), 200);
+                    }, function () {
+                        statusNode.text('清除失败！');
+                        runningStatus = false;
+                        setTimeout(function () {
+                            if (!runningStatus) statusNode.text('清除缓存');
+                        }, 2000);
+                    });
+                });
+            }, function () {
+                statusNode.text('清除失败！');
+                runningStatus = false;
+                setTimeout(function () {
+                    if (!runningStatus) statusNode.text('清除缓存');
+                }, 1000);
+            });
+        });
+    }
+    var li;
+    var userSkin = mw.config.get("skin");
+    if (userSkin === "vector") {
+        li = $('<li id="pt-purge"/>').appendTo("#p-personal ul.vector-menu-content-list");
+        li.append(containerNode);
+    } else if (userSkin === "citizen") {
+        li = $('<li id="purge-cache-button"/>').appendTo("#p-cactions>ul");
+        li.append(containerNode);
+    }
+});
