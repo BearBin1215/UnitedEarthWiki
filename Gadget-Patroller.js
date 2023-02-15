a216950436dea5d92a475abf076d96fce0def9ff
BearBin <460072919@qq.com>
2023-02-15 13:30:52 +0800

    Update Gadget-Patroller.js

--- Gadget-Patroller.js
+++ Gadget-Patroller.js
@@ -1,6 +1,14 @@
-"use strict";
+// <pre>
+'use strict';
+
 $(function () {
+    $('.patroller-only').removeClass('patroller-only');
+
+    var welcomeMessage = '{{欢迎|1=~~~}}'
     if (!mw.config.get('wgPageName').includes('/') && mw.config.get('wgEditMessage') === "creating" && mw.config.get('wgNamespaceIds').user_talk == mw.config.get('wgNamespaceNumber')) {
-        $("#wpTextbox1").val("{{欢迎|~~~}}");
+        $("#wpTextbox1").val(welcomeMessage);
     }
+
 });
+
+// </pre>
