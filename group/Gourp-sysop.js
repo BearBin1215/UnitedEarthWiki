1eafc22d6dd2e2ec5145c45144fd486b4bd51d8d
bearbin1215 <bearbin1215@qq.com>
2025-06-04 10:04:05 +0800

    build(deps): 初步调整依赖

--- group/Gourp-sysop.js
+++ group/Gourp-sysop.js
@@ -3,7 +3,7 @@ $('.patroller-only').removeClass('patroller-only');
 $('.sysop-only').removeClass('sysop-only');
 
 // 一键清理沙盒
-if (mw.config.get("wgPageName") === "帮助:沙盒") {
+if (["帮助:沙盒", "模板:沙盒", "模板:沙盒/style.css"].includes(mw.config.get("wgPageName"))) {
     $(mw.util.addPortletLink("p-cactions", "#", "清理沙盒", "clear-sandbox", "清理沙盒", "r")).on("click", function () {
         new mw.Api().postWithToken("csrf", {
             format: "json",
@@ -11,17 +11,15 @@ if (mw.config.get("wgPageName") === "帮助:沙盒") {
             watchlist: "nochange",
             tags: "Automation tool",
             title: mw.config.get("wgPageName"),
-            text: "<!--请勿删除此行-->{{沙盒页顶}}[[分类:帮助]]<!--Please do not delete this line-->",
+            text: mw.config.get("wgPageName") === "模板:沙盒/style.css" ? "/* [[Category:位于模板名字空间下的CSS页面]] */" : "<!--请勿删除此行-->{{沙盒页顶}}[[分类:帮助]]<!--Please do not delete this line-->",
             summary: "清理沙盒"
+        }).done(function () {
+            mw.notify("清理完毕，即将刷新……");
+            setTimeout(function () {
+                location.reload();
+            }, 2000);
+        }).fail(function (err) {
+            mw.notify("清理沙盒时出现错误：" + err + "。");
         })
-            .done(function () {
-                mw.notify("清理完毕，即将刷新……");
-                setTimeout(function () {
-                    location.reload();
-                }, 2000);
-            })
-            .fail(function (err) {
-                mw.notify("清理沙盒时出现错误：" + err + "。");
-            })
     });
 }
